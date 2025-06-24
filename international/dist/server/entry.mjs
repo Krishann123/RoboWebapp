42efa4532bd070179f0952505df0f9ea7b5785ea
Krishann123 <krishannmillares@gmail.com>
2025-06-24 10:46:17 +0800

    Fixed page management system css issues

--- international/dist/server/entry.mjs
+++ international/dist/server/entry.mjs
@@ -1,6 +1,6 @@
 import { renderers } from './renderers.mjs';
 import { c as createExports, s as serverEntrypointModule } from './chunks/_@astrojs-ssr-adapter_lxgTgEM0.mjs';
-import { manifest } from './manifest_N7qsPnOg.mjs';
+import { manifest } from './manifest_9OkCFEEg.mjs';
 
 const serverIslandMap = new Map();;
 
